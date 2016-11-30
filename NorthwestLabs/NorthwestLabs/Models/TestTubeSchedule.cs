d565daf86a7ca90eee1aeac7f37b857fdc670f29
empick94 <epickard94@gmail.com>
2016-11-30 14:27:48 -0700

    Run Assays Views
    Almost done, but need jacob's stuff

---
+++ NorthwestLabs/NorthwestLabs/Models/TestTubeSchedule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorthwestLabs.Models
+{
+    public class TestTubeSchedule
+    {
+        public TestTube testTube { get; set; }
+        public Schedule schedule { get; set; }
+    }
+}
